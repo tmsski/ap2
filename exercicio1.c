2be2400a1deb29889e17f159c1c86e8f06fe944e
tmsski <20782956+tmsski@users.noreply.github.com>
2023-02-10 23:51:35 +0000

    Primeira aula de laboratório

---
+++ exercicio1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() 
+{
+    printf("Exercício 1\n");
+    int y = 4;
+    float x = sqrt(y);
+
+    printf("x = %f", x);
+}
---
+++ hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Algoritmos e Programação II\n");
+    int idade;
+    printf("Digite sua idade: ");
+    scanf("%d", &idade);
+    int x = idade + 1;
+    printf("Ano que vem você terá %d anos!\n", x);
+}
